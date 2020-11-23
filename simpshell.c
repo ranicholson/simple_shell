b3d344ea6ff28a8be1a6af35b07e5422c4c7227f
ranicholson <ranicholson@git.com>
2020-11-22 20:20:42 -0600

    Updated function and betty compliance

--- simpshell.c
+++ simpshell.c
@@ -1,40 +1,51 @@
 #include "holberton.h"
-
-int main(void)
+/**
+ * main - entry point of the shell, checks interactivity
+ * @argc: arg count
+ * @argv: array of arguments
+ *
+ * Return: 0 on success or other integer
+ */
+int main(int argc, char *argv[])
 {
 	size_t bufsize = 0;
 	char *line = NULL;
-	int i, num_tokens;
+	int i, num_tokens = 0, failcount = 1, shell_terminal, shell_interaction;
 
+	signal(SIGINT, SIG_IGN);
+	/* Check interactivity */
 	shell_terminal = STDIN_FILENO;
 	shell_interaction = isatty(shell_terminal);
-
-	if (shell_interaction == 0)
+	/* if non-interactive */
+	if (shell_interaction == 0 && argc == 1)
 	{
-		i = getline(&line, &bufsize, stdin);
-	        if (i < 0)
+		i = getline(&line, &bufsize, stdin); /* grabs input */
+		if (i < 0)
 		{
-			free(line);
+			free(line); /* frees and returns on failed read */
 			return (-1);
 		}
-		num_tokens = numcount(line);
-		parse(line, num_tokens);
+		num_tokens = numcount(line); /* counts the # of tokens */
+		parse(line, num_tokens, argv, failcount);
+		/* sends line and numtoken to parser */
 	}
+	/* if interactive */
 	while (shell_interaction)
 	{
-		printf("($) ");
-		num_tokens = 0;
-	        i = getline(&line, &bufsize, stdin);
+		/* Shell prompt */
+		write(1, "($) ", 4);
+		num_tokens = 0; /* resets token count to 0 */
+		i = getline(&line, &bufsize, stdin); /* grabs input */
 		if (i < 0)
 		{
 			free(line);
-			printf("\n");
+			write(1, "\n", 1);
+			/* frees, skips line, and repeats for failed input or CTRL - D */
 			break;
 		}
-		num_tokens = numcount(line);
-		if (parse(line, num_tokens) == 1)
-			break;
-		line = NULL;
-	}
+		num_tokens = numcount(line); /* counts tokens */
+		failcount += parse(line, num_tokens, argv, failcount);
+		/* sends line and numtoken to parser - tracks fails*/
+		line = NULL; /* resets line to null */}
 	return (0);
 }
