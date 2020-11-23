0d08b71e1b163ba6fac17aef48bd63da7abb74c6
ranicholson <ranicholson@git.com>
2020-11-22 20:26:42 -0600

    Updating file and betty compliance

--- builtins.c
+++ builtins.c
@@ -1,19 +1,52 @@
 #include "holberton.h"
-
-int checkbltin(char **ar)
+/**
+ * checkbltin - checks if the command entered is a built-in
+ * @line: input entered by user
+ * @ar: array that can be handled by execve
+ * @newline: duplicate of line
+ * @array: Tokens to check
+ * Return: Return depends upon if the built-in was found
+ */
+int checkbltin(char *line, char **ar, char *newline, char **array)
 {
 	int i = 0;
+	char *homeval = NULL;
+	char *home = NULL;
 
-	if (_strcmp(ar[0], "exit") == 0)
-		return (1);
-	if (_strcmp(ar[0], "env") == 0)
+	if (_strcmp(ar[0], "exit") == 0) /* compares first token to exit */
+	{
+		myfree(line, ar, newline, array);
+		_exit(0); /* exits with status 0 */
+	}
+	if (_strcmp(ar[0], "env") == 0) /* compares first token to env */
 	{
 		while (environ[i] != NULL)
 		{
-			printf("%s\n", environ[i]);
+			_puts(environ[i]); /* prints string of environ */
+			write(1, "\n", 1);
 			i++;
 		}
 		return (2);
 	}
+	if (_strcmp(ar[0], "cd") == 0) /* compares first token to exit */
+	{
+		if (ar[1] == NULL)
+		{ /* if cd is by itself */
+			for (i = 0; environ[i] != NULL; i++) /* loops through environ */
+			{
+				if (_strncmp("HOME=", environ[i], 5) == 0)
+				{ /* find the line matching home */
+					home = _strdup(environ[i]);
+					strtok(home, "="); /* stores its value */
+					homeval = strtok(NULL, "=");
+					break;
+				}
+			}
+		}
+		else
+			homeval = ar[1]; /* homeval is set to 2nd arg */
+		chdir(homeval); /* change directory to homeval */
+		free(home);
+		return (2); /* go back to shell loop */	}
 	return (0);
 }
